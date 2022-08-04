62961084e692b6443d0facb3f4df2aa77e713c43
yersel500 <yersel500@gmail.com>
2022-08-04 07:51:25 -0500

    implement updateListing

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -113,4 +113,13 @@ contract NftMarketplace is ReentrancyGuard {
     delete (s_listings[nftAddress][tokenId]);
     emit ItemCanceled(msg.sender, nftAddress, tokenId);
   }
+
+  function updateListing(
+    address nftAddress,
+    uint256 tokenId,
+    uint256 newPrice
+  ) external isListed(nftAddress, tokenId) isOwner(nftAddress, tokenId, msg.sender) {
+    s_listings[nftAddress][tokenId].price = newPrice;
+    emit ItemListed(msg.sender, nftAddress, tokenId, newPrice);
+  }
 }
