7c1ada62c7d62259fa2f8237aa81780e79fc634c
yersel500 <yersel500@gmail.com>
2022-08-03 22:56:19 -0500

    add listing feature

---
+++ contracts/NftMarketplace.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+error NftMarketplace__PriceMustBeAboveZero();
+error NftMarketplace__NotApprovedForMarketplace();
+error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__NotOwner();
+
+contract NftMarketplace {
+  struct Listing {
+    uint256 price;
+    address seller;
+  }
+
+  event ItemListed(
+    address indexed seller,
+    address indexed nftAddress,
+    uint256 indexed tokenId,
+    uint256 price
+  );
+
+  mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+  modifier notListed(
+    address nftAddress,
+    uint256 tokenId,
+    address owner
+  ) {
+    Listing memory listing = s_listings[nftAddress][tokenId];
+    if (listing.price > 0) {
+      revert NftMarketplace__AlreadyListed(nftAddress, tokenId);
+    }
+    _;
+  }
+
+  modifier isOwner(
+    address nftAddress,
+    uint256 tokenId,
+    address spender
+  ) {
+    IERC721 nft = IERC721(nftAddress);
+    address owner = nft.ownerOf(tokenId);
+    if (spender != owner) {
+      revert NftMarketplace__NotOwner();
+    }
+    _;
+  }
+
+  function listItem(
+    address nftAddress,
+    uint256 tokenId,
+    uint256 price
+  ) external notListed(nftAddress, tokenId, msg.sender) isOwner(nftAddress, tokenId, msg.sender) {
+    if (price <= 0) {
+      revert NftMarketplace__PriceMustBeAboveZero();
+    }
+    IERC721 nft = IERC721(nftAddress);
+    if (nft.getApproved(tokenId) != address(this)) {
+      revert NftMarketplace__NotApprovedForMarketplace();
+    }
+    s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+    emit ItemListed(msg.sender, nftAddress, tokenId, price);
+  }
+}
