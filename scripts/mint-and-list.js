2c03e8250a902328e042579a674abd1a610dfce3
yersel500 <yersel500@gmail.com>
2022-08-05 20:27:07 -0500

    provide to the front end the number of contract

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,4 +1,5 @@
-const { ehters, ethers } = require("hardhat")
+const { ethers, network } = require("hardhat")
+const { moveBlocks } = require("../utils/move-blocks")
 
 const PRICE = ethers.utils.parseEther("0.1")
 
@@ -16,6 +17,10 @@ async function mintAndList() {
   const tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE)
   await tx.wait(1)
   console.log("Listed!")
+
+  if (network.config.chainId == 31337) {
+    await moveBlocks(1, (sleepAmount = 1000))
+  }
 }
 
 mintAndList()
